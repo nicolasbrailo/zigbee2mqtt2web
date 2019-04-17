332dcd8f6f228e7d35a52957ca821a74da1d3b19
Nico <nicolasbrailo@gmail.com>
2019-04-17 21:19:57 +0200

    Split own config from generic module

--- zigbee2mqtt2flask/__init__.py
+++ zigbee2mqtt2flask/__init__.py
@@ -2,6 +2,7 @@ from .thing_registry import ThingRegistry
 from .mqtt_proxy import MqttProxy
 
 from flask import send_from_directory
+import os
 
 class Zigbee2Mqtt2Flask(object):
     """ Interface between Zigbee/Mqtt and Flask: objects implementing the Thing
@@ -13,10 +14,19 @@ class Zigbee2Mqtt2Flask(object):
         self.thing_registry = ThingRegistry(flask_app, flask_endpoint_prefix)
         self.mqtt = MqttProxy(mqtt_ip, mqtt_port, mqtt_topic_prefix, [self.thing_registry])
 
-        print("Registered {} for webapp endpoint".format('/' + flask_endpoint_prefix + '/webapp/<path:path>'))
-        @flask_app.route('/' + flask_endpoint_prefix + '/webapp/<path:path>')
-        def flask_endpoint_webapp_root(path):
-            return send_from_directory('zigbee2mqtt2flask/webapp', path)
+        try:
+            cwd = os.path.dirname(os.path.abspath(__file__))
+        except:
+            cwd = ''
+
+        # Need to use absolute path in case someone is using this as a module
+        filesys_path_to_webdir = cwd + '/webapp'
+        print("Will serve Zigbee2Mqtt2Flask webapp from {}".format(filesys_path_to_webdir))
+        print("Registered {} for webapp endpoint".format('/' + flask_endpoint_prefix + '/webapp/<path:urlpath>'))
+
+        @flask_app.route('/' + flask_endpoint_prefix + '/webapp/<path:urlpath>')
+        def flask_endpoint_webapp_root(urlpath):
+            return send_from_directory(filesys_path_to_webdir, urlpath)
 
 
     def start_mqtt_connection(self):
