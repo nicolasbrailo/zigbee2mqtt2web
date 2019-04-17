4b0b5c4c232370b1e9121607b47517fa4e9dbab8
Nico <nicolasbrailo@gmail.com>
2019-04-17 22:43:18 +0200

    Refactor

--- sample_server.py
+++ sample_server.py
@@ -1,12 +1,17 @@
 from zigbee2mqtt2flask.things import ColorDimmableLamp, Button
 from zigbee2mqtt2flask import Zigbee2Mqtt2Flask
+from zigbee2mqtt2flask.zigbee2mqtt2flask.mqtt_web_socket_streamer import MqttToWebSocket
+
 from flask import Flask
 from flask_socketio import SocketIO
 
 flask_app = Flask(__name__)
-flask_socketio = SocketIO(flask_app)
 mqtt_ip, mqtt_port, mqtt_topic_prefix = '192.168.2.100', 1883, 'zigbee2mqtt/'
 world = Zigbee2Mqtt2Flask(flask_app, 'ZMF', mqtt_ip, mqtt_port, mqtt_topic_prefix)
+
+# Optional: Add streaming mqtt log endpoints (see http://127.0.0.1:2000/ZMF/webapp/mqtt_log_example.html)
+flask_socketio = SocketIO(flask_app)
+MqttToWebSocket.build_and_register(flask_socketio, world)
 
 # Describe things and actions
 class MyButton(Button):
@@ -21,20 +26,6 @@ class MyButton(Button):
 
 world.register_thing(ColorDimmableLamp('MyDeskLamp', 'MyDeskLamp', world.mqtt))
 world.register_thing(MyButton('MyButton', 'MyButton', world))
-
-
-# Add a streaming mqtt log
-class MqttToWebSocket(object):
-    def on_thing_message(self, thing_id, topic, parsed_msg):
-        flask_socketio.emit('mqtt-thing-message', 
-                {'thing': thing_id, 'topic': topic, 'msg': parsed_msg})
-
-    def on_unknown_message(self, topic, payload):
-        flask_socketio.emit('non-understood-mqtt-message',
-                {'topic': topic, 'msg': str(payload.decode('utf-8'))})
-
-world.set_mqtt_listener(MqttToWebSocket())
-
 
 # Start the world. Check http://127.0.0.1:2000/ZMF/webapp/example_app.html
 world.start_mqtt_connection()
