4c98505fee6054fc05aceaf00cb5db99f5d67b88
Nico Brailovsky <nicolasbrailo@gmail.com>
2023-03-16 19:22:10 +0000

    Make all browser deps local only

---
+++ zigbee2mqtt2web_ui/localize_js_deps.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+set -euo pipefail
+
+DEPS_DEST_PATH="www/extjsdeps"
+DEPS_READ_FROM="www/index.html"
+
+rm -rf "$DEPS_DEST_PATH"
+mkdir -p "$DEPS_DEST_PATH"
+
+for jsdep in $(cat "$DEPS_READ_FROM" | grep 'src="https://' | awk -F'src=' '{print $2}' | tr -d '"'); do
+  echo "Localize $jsdep"
+  wget --directory-prefix="$DEPS_DEST_PATH" $jsdep
+done
+
+echo "Copypaste:"
+for jsdep in "$DEPS_DEST_PATH"/*.js; do
+  echo "<script src='$jsdep'></script>"
+done
+
