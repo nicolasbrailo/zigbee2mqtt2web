01c35ac41abde861c222059d22d85847e447e94e
Nico B <nicolasbrailo@gmail.com>
2024-01-16 09:18:17 +0000

    Add non-networked system message bus

--- zigbee2mqtt2web/__init__.py
+++ zigbee2mqtt2web/__init__.py
@@ -7,12 +7,16 @@ from .zigbee2mqtt_bridge import Zigbee2MqttBridge
 from .zigbee2mqtt_thing import Zigbee2MqttAction
 from .zigbee2mqtt_thing import Zigbee2MqttActionValue
 
+import threading
+import time
 import logging
-logger = logging.getLogger(__name__)
+log = logging.getLogger(__name__)
 
 logging.getLogger('werkzeug').setLevel(logging.ERROR)
 logging.getLogger('apscheduler.scheduler').setLevel(logging.ERROR)
 
+
+TIME_TO_CONSIDER_CB_SLOW_MS = 5
 
 class Zigbee2Mqtt2Web:
     """ Global wrapper for Z2M2W """
@@ -29,6 +33,9 @@ class Zigbee2Mqtt2Web:
         self._mqtt_topic_zmw = cfg['mqtt_topic_zmw'] if 'mqtt_topic_zmw' in cfg else None
         self._mqtt_registry = Zigbee2MqttBridge(cfg, self._mqtt)
         self._mqtt_registry.on_mqtt_network_discovered(self._monkey_patch)
+        # Besides MQTT, offer a system bus for high priority messages
+        self._system_evt_subscriber = []
+        self._announcement_threads = []
         self.registry = ThingRegistry(self._mqtt_registry)
         self.webserver = FlaskBridge(cfg, self.registry)
 
@@ -45,20 +52,44 @@ class Zigbee2Mqtt2Web:
         """ Add a rule to be called when a network discovery message is published """
         self._monkeypatch_rules.append((name, matcher, callback))
 
+    def subscribe_to_system_events(self, cb):
+        self._system_evt_subscriber.append(cb)
+
     def announce_system_event(self, json_like_map_evt):
+        def do_announce():
+            for cb in self._system_evt_subscriber:
+                start_time = time.time()
+                cb(json_like_map_evt)
+                dt = time.time() - start_time
+                if dt > TIME_TO_CONSIDER_CB_SLOW_MS:
+                    log.info("Caution: system event listener %s is slow/blocking, running for t=%s", cb, dt)
+
+        bgt = threading.Thread(target=do_announce)
+        bgt.start()
+
         if self._mqtt_topic_zmw is None:
-            logger.critical('Requested system announcement, but feature is disabled.'
+            log.critical('Requested system announcement, but feature is disabled.'
                             'Announcement: %s', json_like_map_evt)
         else:
-            logger.debug('Broadcasting system event: %s', json_like_map_evt)
+            log.debug('Broadcasting system event: %s', json_like_map_evt)
             self._mqtt.broadcast(self._mqtt_topic_zmw, json_like_map_evt)
+
+        self._announcement_threads.append(bgt)
+        new_announcement_threads = []
+        for thr in self._announcement_threads:
+            if thr.is_alive():
+                new_announcement_threads.append(thr)
+            else:
+                thr.join()
+        self._announcement_threads = new_announcement_threads
+
 
     def _monkey_patch(self):
         for name in self.registry.get_thing_names():
             thing = self.registry.get_thing(name)
             for patch_name, matches, patch in self._monkeypatch_rules:
                 if thing.is_zigbee_mqtt and matches(thing):
-                    logger.info(
+                    log.info(
                         'Applying patch to %s: %s',
                         thing.name,
                         patch_name)
