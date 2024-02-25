cd714aea3af8c1c61a166e4c41d986a5006c840b
Nico B <nicolasbrailo@gmail.com>
2024-02-25 18:58:02 +0000

    Add redshift helper

--- zigbee2mqtt2web_extras/utils/geo_helper.py
+++ zigbee2mqtt2web_extras/utils/geo_helper.py
@@ -29,3 +29,34 @@ def late_night(latlon, late_night_start_hour):
         if local_hour >= late_night_start_hour or local_hour <= next_sunrise.hour:
             return True
     return False
+
+# How many discrete steps the redshift process will have
+REDSHIFT_STEPS_CNT = 5
+# Redshfit is done from sunset to dusk * mult. Eg if dusk-sunset = 1h and mult=2, then redshift
+# will be done in a period of 2 hours
+REDSHIFT_DURATION_MULT = 2
+# Redshift will start at sunset-(duration*offset). Eg if sunset=17.00, duration=1h and offset=.5,
+# then redshift will start at 16.30 and last until 17.30
+REDSHIFT_START_OFFSET = .7
+
+# Offer a redshift % range, to allow excluding way too bright settings
+REDSHIFT_START_PCT = 20
+REDSHIFT_END_PCT = 80
+
+def todays_redshift_steps(latlon, on_date=datetime.date.today()):
+    """ Return a list of times and %s to gradually redshift ligths as sun goes down """
+    # The time in the evening when the sun is about to disappear below the horizon (asuming a location with no obscuring features)
+    sunset = astral_sun(astral.Observer(*latlon), date=on_date)['sunset']
+    # The time in the evening when the sun is a specific number of degrees below the horizon.
+    dusk = astral_sun(astral.Observer(*latlon), date=on_date)['dusk']
+
+    redshift_duration = REDSHIFT_DURATION_MULT * (dusk - sunset)
+    redshift_start = sunset - (REDSHIFT_START_OFFSET * redshift_duration)
+    step_duration = redshift_duration / REDSHIFT_STEPS_CNT
+    redshift_range = REDSHIFT_END_PCT - REDSHIFT_START_PCT
+    redshift_steps = []
+    for i in range(1, REDSHIFT_STEPS_CNT+1):
+        red_pct = REDSHIFT_START_PCT + (redshift_range * i / REDSHIFT_STEPS_CNT)
+        t = redshift_start + i*step_duration
+        redshift_steps.append((t, red_pct))
+    return redshift_steps
